4b3ced5cd2db60808f0cfc1cdc9a0c6b7e907010
André D Oliveira <decaobr@gmail.com>
2018-04-17 22:34:16 -0300

    Chegando no passo 6

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//alterar na página about.ts
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-about',
+  templateUrl: 'about.html'
 })
-export class HomePage {
+export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
